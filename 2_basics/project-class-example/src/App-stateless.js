5a9c2a071af2a4c95b7aa14b0235289702242c6f
Eduardo <eduardo.benigno15@gmail.com>
2021-06-08 13:57:05 -0300

    starting classes components

---
+++ 2_basics/project-class-example/src/App-stateless.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {Component} from 'react';
+
+//react class component
+class App extends Component{
+  //for now, this component is stateless (without hooks/states)
+
+
+  //inside this class, there's always a method called render
+  render (){
+    return(
+      <p>a</p>
+    )
+  }
+}
+
+export default App;
--- 2_basics/project-class-example/src/App.js
+++ 2_basics/project-class-example/src/App.js
@@ -3,13 +3,43 @@ import {Component} from 'react';
 
 //react class component
 class App extends Component{
-  //for now, this component is stateless (without hooks/states)
+  constructor(props){
+    //super is to call the constructor of "Component" class
+    super(props);
 
+    //Manually setting bind to allow handleClick function to use this from state
+    this.handleClick = this.handleClick.bind(this);
+
+    //Also, is possible to not use constructor just putting this code alone
+    this.state = {
+      name: "Eduardo",
+      counter: 0
+    }
+  }
+
+  //This common function must have bind as seen in line 11
+  handleClick(){
+    this.setState({name: "Vinícius"})
+  }
+
+  //This function with arrow doesn't need bind as seen in line 11
+  handleClickTwo = () =>{
+    const {counter} = this.state;
+    this.setState({ counter: counter + 1})
+  }
 
   //inside this class, there's always a method called render
   render (){
+
+    //Destructuring var from state
+    const {name, counter} = this.state
+
     return(
-      <h1>a</h1>
+      <>
+        <p>Name: {name}</p>
+        <button onClick={this.handleClick}>Change my name</button>
+        <button onClick={this.handleClickTwo}>Counter: {counter}</button>
+      </>
     )
   }
 }
